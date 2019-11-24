7b75994f4fbca88c8164507b917c4d20a4b92dae
Jonathan Bac <36225255+j-bac@users.noreply.github.com>
2019-11-24 23:34:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
 
     keywords='machine_learning graphs dimension_reduction single_cell',  # Optional
-    packages=['elpigraph'],
+    packages=['elpigraph','elpigraph.src'],
 #     package_dir={'elpigraph': 'elpigraph'},
     package_data={'elpigraph': ['data/*.csv']},
     install_requires=['numpy','pandas','scipy','scikit_learn','python_igraph','plotnine'],
