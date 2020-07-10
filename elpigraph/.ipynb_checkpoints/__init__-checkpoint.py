8111aa66ef43f76ae0bf8396b45bd686e81bfa9a
j-bac <jo.bac@live.fr>
2020-07-10 18:53:48 +0200

    update tests

--- elpigraph/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from ._AlterStructure import ExtendLeaves, CollapseBranches, ShiftBranching
-from ._BaseElPiWrapper import computeElasticPrincipalGraphWithGrammars
-from ._EMAdjustment import AdjustByConstant
-from ._plotting import PlotPG	
-from ._topologies import computeElasticPrincipalCircle,computeElasticPrincipalTree,computeElasticPrincipalCurve,finetuneBR,GrowLeaves,generateInitialConfiguration
-from . import src
