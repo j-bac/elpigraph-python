aea4cd0ce9ab8f2944c7bbef8b820bec22efb1ee
Jonathan Bac <36225255+j-bac@users.noreply.github.com>
2022-05-05 18:27:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="elpigraph-python",  # Required
-    version="0.1.1",  # Required
+    version="0.2.0",  # Required
     description="",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
